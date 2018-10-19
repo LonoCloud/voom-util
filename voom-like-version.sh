ce47a3106b83f759eef676c4581a74ff7c01d84c
Chouser <chris.houser@viasat.com>
2018-10-19 01:43:45 +0000

    [voom-util/voom-like-version] don't allow ambiguous directory option

--- voom-like-version.sh
+++ voom-like-version.sh
@@ -8,14 +8,22 @@ voom_version() {
     echo $(date --date="$(git log -1 --pretty=%ci -- "$@")" "+%Y%m%d_%H%M%S")-g$(git log -1 --pretty=%h -- "$@")$(test -z "$(git status --short -- "$@")" || echo _DIRTY)
 }
 
+usage() {
+    echo >&2 "usage: $0 <PATH> [PATH...] # Optionally, set REPO_ROOT_VOOM instead of supplying <PATH>."
+    exit 1
+}
+
 
 if [ -z "$*" ]; then
     if [ "$REPO_ROOT_VOOM" ]; then
         voom_version
     else
-        echo >&2 "usage: $0 <PATH> [PATH...] # Optionally, set REPO_ROOT_VOOM instead of supplying <PATH>."
-        exit 1
+        usage
+    fi
+else
+    if [ "$REPO_ROOT_VOOM" ]; then
+        usage
+    else
+        voom_version "$@"
     fi
 fi
-
-voom_version "$@"
